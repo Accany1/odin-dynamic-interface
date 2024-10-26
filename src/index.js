3fc219f6771d31b4c17c62694c3ef05d20c0e262
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-10-26 14:58:27 +0800

    Carousel now works with buttons

--- src/index.js
+++ src/index.js
@@ -5,3 +5,34 @@ document.querySelector(".dropbtn").addEventListener("click", () => {
 }
 );
 
+
+const ImageCarousel = () => {
+    const images = document.querySelectorAll(".image-container")
+    const prev = document.querySelector(".prev")
+    const next = document.querySelector(".next")
+    let currentImage = 0
+    images[currentImage].style.display = "block"
+    prev.addEventListener("click", () => {
+        currentImage--
+        if (currentImage < 0) {
+            currentImage = images.length - 1
+        }
+        for (let i = 0; i < images.length; i++) {
+            images[i].style.display = "none"
+        }
+        images[currentImage].style.display = "block"
+        
+    })
+    next.addEventListener("click", () => {
+        currentImage++
+        if (currentImage > images.length - 1) {
+            currentImage = 0
+        }
+        for (let i = 0; i < images.length; i++) {
+            images[i].style.display = "none"
+        }
+        images[currentImage].style.display = "block"
+    })
+}
+
+ImageCarousel()
